e0e62d99caa9c14a33ac69533afc10b0c7de5362
zub165 <95563574+zub165@users.noreply.github.com>
2025-02-17 21:45:32 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    GITHUB_TOKEN: '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', // Add your GitHub token here (keep it private)
+    GOOGLE_MAPS_KEY: 'AIzaSyCI_RHBNmNwpNpDoUPyZFNPNWUYQmr4QJk', // Add your Google Maps API key
+    REPO_OWNER: 'zub165',
+    REPO_NAME: 'er-wait-time'
+}; 
